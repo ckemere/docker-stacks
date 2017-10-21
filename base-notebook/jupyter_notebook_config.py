c81054c61b622a56aa20391b8e69dcfda62aa4ed
Caleb Kemere <ckemere@gmail.com>
2017-10-21 12:57:08 -0500

    Modifications to enable dask and nelpy and update network security

--- base-notebook/jupyter_notebook_config.py
+++ base-notebook/jupyter_notebook_config.py
@@ -8,29 +8,31 @@ import errno
 import stat
 
 c = get_config()
-c.NotebookApp.ip = '*'
-c.NotebookApp.port = 8888
+c.NotebookApp.password = u'sha1:e6888af5df44:e161f2e90fb7b03c52e979dfcf18f10353caef22'
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.token = ''
 c.NotebookApp.open_browser = False
+c.NotebookApp.port = 8888
 
 # Generate a self-signed certificate
-if 'GEN_CERT' in os.environ:
-    dir_name = jupyter_data_dir()
-    pem_file = os.path.join(dir_name, 'notebook.pem')
-    try:
-        os.makedirs(dir_name)
-    except OSError as exc:  # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(dir_name):
-            pass
-        else:
-            raise
-    # Generate a certificate if one doesn't exist on disk
-    subprocess.check_call(['openssl', 'req', '-new',
-                           '-newkey', 'rsa:2048',
-                           '-days', '365',
-                           '-nodes', '-x509',
-                           '-subj', '/C=XX/ST=XX/L=XX/O=generated/CN=generated',
-                           '-keyout', pem_file,
-                           '-out', pem_file])
-    # Restrict access to the file
-    os.chmod(pem_file, stat.S_IRUSR | stat.S_IWUSR)
-    c.NotebookApp.certfile = pem_file
+dir_name = jupyter_data_dir()
+pem_file = os.path.join(dir_name, 'notebook.pem')
+try:
+  os.makedirs(dir_name)
+except OSError as exc:  # Python >2.5
+  if exc.errno == errno.EEXIST and os.path.isdir(dir_name):
+    pass
+  else:
+    raise
+# Generate a certificate if one doesn't exist on disk
+subprocess.check_call(['openssl', 'req', '-new',
+		   '-newkey', 'rsa:2048',
+		   '-days', '365',
+		   '-nodes', '-x509',
+		   '-subj', '/C=XX/ST=XX/L=XX/O=generated/CN=generated',
+		   '-keyout', pem_file,
+		   '-out', pem_file])
+# Restrict access to the file
+os.chmod(pem_file, stat.S_IRUSR | stat.S_IWUSR)
+c.NotebookApp.certfile = pem_file
+
